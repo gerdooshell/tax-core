a312fc9d2ddcd989c76f11cee6df2501108383ef
Hesam Eskandari <eskandarihesam@gmail.com>
2024-02-17 13:26:49 -0800

    [debug] check for null input

--- interactors/controller_access/canada_tax_margin_calculator/implementation.go
+++ interactors/controller_access/canada_tax_margin_calculator/implementation.go
@@ -2,6 +2,8 @@ package canadaTaxMarginCalculator
 
 import (
 	"context"
+	"errors"
+
 	canadaTaxMrgins "github.com/gerdooshell/tax-core/interactors/internal/tax_margins/canada"
 	marginDS "github.com/gerdooshell/tax-core/interactors/internal/tax_margins/canada/data_structures"
 )
@@ -14,6 +16,10 @@ func NewCanadaTaxMarginCalculator() CanadaTaxMarginCalculator {
 }
 
 func (c canadaTaxMarginCalculator) GetAllMarginalBrackets(ctx context.Context, input *Input) (out Output, err error) {
+	if input == nil {
+		err = errors.New("null marginal input")
+		return
+	}
 	margin := canadaTaxMrgins.NewTaxMarginCa()
 	brackets, err := margin.GetCombinedMarginalBrackets(ctx, marginDS.Input{Year: input.Year, Province: input.Province})
 	if err != nil {
