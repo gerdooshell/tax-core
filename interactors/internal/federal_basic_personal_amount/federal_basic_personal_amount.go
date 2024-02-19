013e85a26ff61cf58ef873e373ac4108bb5c899b
Hesam Eskandari <eskandarihesam@gmail.com>
2024-02-19 11:43:16 -0800

    [SMK-100] dev federal bpa interactor

---
+++ interactors/internal/federal_basic_personal_amount/federal_basic_personal_amount.go
@@ -0,0 +1,47 @@
+package federalBPA
+
+import (
+	"context"
+
+	dataProvider "github.com/gerdooshell/tax-core/data-access"
+	dataAccess "github.com/gerdooshell/tax-core/interactors/data_access"
+)
+
+type FederalBasicPersonalAmountInteractor interface {
+	GetFederalBPA(ctx context.Context, year int, totalIncome float64) <-chan FederalBasicPersonalAmountOutput
+}
+
+type FederalBasicPersonalAmountOutput struct {
+	Value float64
+	Err   error
+}
+
+func NewFederalBasicPersonalAmountInteractor() FederalBasicPersonalAmountInteractor {
+	return &federalBPAImpl{
+		dataProvider: dataProvider.GetDataProviderServiceInstance(),
+	}
+}
+
+type federalBPAImpl struct {
+	dataProvider dataAccess.FederalBPAData
+}
+
+func (f *federalBPAImpl) GetFederalBPA(ctx context.Context, year int, totalIncome float64) <-chan FederalBasicPersonalAmountOutput {
+	out := make(chan FederalBasicPersonalAmountOutput)
+	go func() {
+		defer close(out)
+		federalBPAOutput := FederalBasicPersonalAmountOutput{}
+		defer func() { out <- federalBPAOutput }()
+		bpaChan, errChan := f.dataProvider.GetFederalBPA(ctx, year)
+		select {
+		case federalBPAOutput.Err = <-errChan:
+			return
+		case federalBPA := <-bpaChan:
+			if federalBPAOutput.Err = federalBPA.Calculate(totalIncome); federalBPAOutput.Err != nil {
+				return
+			}
+			federalBPAOutput.Value = federalBPA.GetValue()
+		}
+	}()
+	return out
+}
